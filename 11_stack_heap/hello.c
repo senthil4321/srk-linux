fe60e2738728f6e5f4242e226ee9e6b0373539bc
srk <senthil.jogi@gmail.com>
2025-01-19 12:53:10 +0100

    Increase loop iteration in hello.c and remove unused import os.py

--- 11_stack_heap/hello.c
+++ 11_stack_heap/hello.c
@@ -11,7 +11,7 @@ int main(void)
                fprintf(stderr, "Error \n");
                return (-1);
         }
-        for (i = 0; i < 100; i++)
+        for (i = 0; i < 1000; i++)
         {
                 printf("[%d] %s (%p)\n", i, str, &str);
                 sleep(1);
